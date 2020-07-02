c6eca0266b5e51f2cf8e0791c7c2ffcb656fcacb
santiagoRojas <santi_rojas123@hotmail.com>
2020-07-02 18:20:42 -0500

    cambios en conflictos
    se cambio todos losconflictos

--- holamundo/src/holamundo/Holamundo.java
+++ holamundo/src/holamundo/Holamundo.java
@@ -1,36 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package holamundo;
 
-import javax.swing.JOptionPane;
-
-/**
- *
- * @author TOSHIBA
- */
-public class Holamundo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-
-        JOptionPane.showMessageDialog(null, "hola gitkraken");
-        // vamos a agregarle nuevas caracteristicas como pedir un dato
-        String nombre =JOptionPane.showInputDialog("digite su nombre");
-
-        JOptionPane.showMessageDialog(null, "hola GitKraken");
-        //esta es unacorreccion de bug
-        int a = 10;
-
-    }
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -53,7 +21,7 @@ public class Holamundo {
 
         JOptionPane.showMessageDialog(null, "hola GitKraken");
         // vamos a agregarle nuevas caracteristicas como pedir un dato
-        JOptionPane.showInputDialog("digite su nombre");
+         String nombre =JOptionPane.showInputDialog("digite su nombre");
 
         //esta es unacorreccion de bug
         int a = 10;
@@ -61,4 +29,3 @@ public class Holamundo {
     }
     
 }
->>>>>>> master
